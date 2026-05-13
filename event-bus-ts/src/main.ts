459fc1f743444c3ef04529b4b045475676a4caaa
dustin chen <31425837+chenchangchao@users.noreply.github.com>
2026-05-13 20:04:06 +0800

    Implement EventBus with hello-world example

---
+++ event-bus-ts/src/main.ts
@@ -0,0 +1,12 @@
+import { EventBus } from './event-bus';
+
+EventBus.getInstance().register('hello-world', (name: string) => {
+    if(name)
+        console.log('Hello ' + name);
+    else 
+        console.log('Hello world');
+});
+
+EventBus.getInstance().dispatch<string>('hello-world', 'Luis');
+EventBus.getInstance().dispatch<string>('hello-world');
+EventBus.getInstance().dispatch<string>('hello-world');
